f85674be25f99493277a0ede18f4907b295572da
forbidTeller <ftattoo4780@gmail.com>
2026-08-29 10:03:29 +0800

    Fixing coding style...

--- src/quiz_generator/QuizGenerator.java
+++ src/quiz_generator/QuizGenerator.java
@@ -4,7 +4,7 @@ import quiz_generator.Frontend.Start;
 
 public class QuizGenerator 
 { 
-    public static void main (String[] args) 
+    public static void main(String[] args) 
     {
         System.setProperty("sun.java2d.uiScale", "1.0");
         
